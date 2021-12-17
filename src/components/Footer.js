f143ebeb717c06123ef11e2d65db4812e99349d3
Colli Gregory <colli.gregory.13@gmail.com>
2021-12-17 18:17:57 +0100

    enleve fontawesome local au  cdn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 
 export const Footer = () => {
     return (
         <footer>
+            <Link className="navLink go-top"
+            activeClass="active"
+            to="header"
+            spy={true}
+            smooth={true}
+            duration={500}>
+                <i className="alignIcon fas fa-sort-up"></i>
+            </Link>
             <div>
-                github
+                <a target='_blank' href='https://github.com/greg13013'><i className="fab fa-github"></i></a>
             </div>
         </footer>
     )
